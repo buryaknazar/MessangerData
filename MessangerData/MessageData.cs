a48d550ecd0704091062dd7024484051d648a5e2
Nazar Buryak <nazarburyak@ukr.net>
2024-05-09 11:57:55 +0300

    Add project files.

---
+++ MessangerData/MessageData.cs
@@ -0,0 +1,14 @@
+﻿namespace MessangerData
+{
+    [Serializable]
+    public class MessageData
+    {
+        public string From { get; set; } //sender
+        public string To { get; set; } //receiver
+        public string Action { get; set; } //verbs
+        public string Text { get; set; } //text message
+        public string? FileName { get; set; } //file name
+        public byte[] FileData { get; set; } //file data
+        public DateTime CreatedAt { get; set; } // creation time
+    }
+}
